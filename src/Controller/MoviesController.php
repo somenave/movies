3dfbbae2bbea7e1427a1d296d8a868965d759912
ksenia <gunichdrabl@gmail.com>
2023-09-10 23:50:24 +0300

    WIP: crud movies templates

--- src/Controller/MoviesController.php
+++ src/Controller/MoviesController.php
@@ -2,25 +2,37 @@
 
 namespace App\Controller;
 
-use App\Entity\Movie;
-use Doctrine\ORM\EntityManagerInterface;
+use App\Repository\MovieRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class MoviesController extends AbstractController
 {
-    private $em;
-    public function __construct(EntityManagerInterface $em)
+    private $movieRepository;
+    public function __construct(MovieRepository $movieRepository)
     {
-        $this->em = $em;
+        $this->movieRepository = $movieRepository;
     }
 
-    #[Route('/movies', name: 'app_movies')]
+    #[Route('/movies', methods: ['GET'], name: 'movies')]
     public function index(): Response
     {
-        $repositoy = $this->em->getRepository(Movie::class);
-        $movies = $repositoy->findAll();
+        $movies = $this->movieRepository->findAll();
         return $this->render('movies/index.html.twig', ['movies' => $movies]);
+    }
+
+    #[Route('/movies/{id}', methods: ['GET'], name: 'movie')]
+    public function show($id): Response
+    {
+        $movie = $this->movieRepository->find($id);
+        return $this->render('movies/show.html.twig', ['movie' => $movie]);
+    }
+
+    #[Route('/movies/{id}/edit', methods: ['GET'], name: 'movie_edit')]
+    public function edit($id): Response
+    {
+        $movie = $this->movieRepository->find($id);
+        return $this->render('movies/edit.html.twig', ['movie' => $movie]);
     }
 }
