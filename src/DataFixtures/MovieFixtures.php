79440fed72301ad2df6eb7ea22db0e03086f243e
ksenia <gunichdrabl@gmail.com>
2023-09-10 15:42:56 +0300

    feat: add data fixtures

---
+++ src/DataFixtures/MovieFixtures.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Movie;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class MovieFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        $movie = new Movie();
+        $movie->setTitle('The Dark Knight');
+        $movie->setReleaseYear(2008);
+        $movie->setDescription('This is the description of the Dark Knight');
+        $movie->setImagePath('https://images.unsplash.com/photo-1531259683007-016a7b628fc3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1974&q=80');
+        $movie->addActor($this->getReference('actor_1'));
+        $movie->addActor($this->getReference('actor_2'));
+        $manager->persist($movie);
+
+        $movie2 = new Movie();
+        $movie2->setTitle('Snatch');
+        $movie2->setReleaseYear(2000);
+        $movie2->setDescription('Unscrupulous boxing promoters, violent bookmakers, a Russian gangster, incompetent amateur robbers and supposedly Jewish jewelers fight to track down a priceless stolen diamond.');
+        $movie2->setImagePath('https://m.media-amazon.com/images/M/MV5BMTA2NDYxOGYtYjU1Mi00Y2QzLTgxMTQtMWI1MGI0ZGQ5MmU4XkEyXkFqcGdeQXVyNDk3NzU2MTQ@._V1_FMjpg_UX1000_.jpg');
+        $movie2->addActor($this->getReference('actor_3'));
+        $movie2->addActor($this->getReference('actor_4'));
+        $manager->persist($movie2);
+
+        $manager->flush();
+    }
+}
